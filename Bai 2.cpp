04dafea97405721d0400b92715632cabaadd1279
Blackhacker-a <64885014+Blackhacker-a@users.noreply.github.com>
2020-05-14 21:04:00 +0700

    Update Bai 2.cpp

--- Bai 2.cpp
+++ Bai 2.cpp
@@ -3,6 +3,10 @@ int main(){
 	int n;
 	printf("Nhap vao so nguyen duong n ");
 	scanf("%d",&n);
+	if(n<0){
+		printf("Hay nhap lai");
+		return 0;
+		}
 	float sum=0;
 	for(int i=1;i<=n;i++){
 	    sum+=(float)1/i;
